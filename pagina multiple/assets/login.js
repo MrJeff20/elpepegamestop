339385989652eff0ff100736a0e4d0bf176dbb77
Seishiro <robe.jara@duocuc.cl>
2025-09-09 18:06:35 -0300

    login que redirecciona al index

---
+++ pagina multiple/assets/login.js
@@ -0,0 +1,12 @@
+function logear(){
+    let user =document.getElementById("usuario").value;
+    let pass =document.getElementById("contraseña").value;
+
+    if(user =="Juan"&&pass=="1"){
+
+        window.location.href = 'index.html';
+    }
+    else{
+        alert("datos incorrectos")
+    }
+}
